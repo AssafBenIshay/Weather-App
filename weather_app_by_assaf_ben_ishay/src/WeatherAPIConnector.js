c859da05e97dcfa7090947360abf727a92150d4d
AssafBenIshay <b.i.app.solutions@gmail.com>
2025-09-25 18:44:53 +0300

    updated to secure HTTPS on api requests

--- weather_app_by_assaf_ben_ishay/src/WeatherAPIConnector.js
+++ weather_app_by_assaf_ben_ishay/src/WeatherAPIConnector.js
@@ -1,7 +1,7 @@
 
 
-const  API_KEY='f96b73be4fc88b657edfebb3a2275109'
-const API_URL = `http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid=${API_KEY}`
+const API_KEY = 'f96b73be4fc88b657edfebb3a2275109'
+const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid=${API_KEY}`
 const BASE_URL = `https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m,precipitation,precipitation_probability&forecast_days=9#`
 const MEAN_URL = `https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=weather_code,temperature_2m_max,temperature_2m_min`
 export const connectWeatherAPI = async (city) => {
@@ -24,7 +24,7 @@ export const getWeatherAPI = async (lat, lon) => {
 }
 
 export const getMeanWeatherAPI = async (lat, lon) => {
-        if(!lat || !lon) return null
+    if (!lat || !lon) return null
 
     const url = MEAN_URL.replace('{lat}', lat.toString()).replace('{lon}', lon.toString())
     const response = await fetch(url);
